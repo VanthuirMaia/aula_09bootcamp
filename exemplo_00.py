f4945a51beea3ede323fd31bf2e760b368cd339c
VanthuirMaia <vanmaiasf@gmail.com>
2025-03-08 22:38:32 -0300

    feat - Iniciando o Logger

---
+++ exemplo_00.py
@@ -0,0 +1,14 @@
+from loguru import logger
+
+logger.add("meu_app.log")
+
+def soma(x, y):
+    logger.info(x)
+    logger.info(y)
+    logger.info(x + y)
+    return x + y
+
+
+resultado = soma(2,"3")
+
+print(resultado)
---
+++ logging_exemplo_erros.py
@@ -0,0 +1,7 @@
+from loguru import logger
+
+logger.debug("Um aviso para o desenvolvedor (ou eu mesmo) no futuro")
+logger.info("Informação importante do processo")
+logger.warning("Um aviso que algo vai oarar de funcionar no futuro")
+logger.error("Acoteceu uma falha")
+logger.critical("Aconteceu uma que aborta a aplicação")
